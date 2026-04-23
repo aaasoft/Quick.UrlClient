be877d39cd1c2caa08f94c12aa8ca38cdf819b54
张鹏 <scbeta@qq.com>
2026-04-23 14:23:01 +0800

    增加一个测试项目

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using Quick.UrlClient;
+using Quick.UrlClient.Pipe;
+using Quick.UrlClient.SerialPort;
+using Quick.UrlClient.Tcp;
+
+TcpUrlClient.Register();
+SerialPortUrlClient.Register();
+PipeUrlClient.Register();
+
+var urlClient = UrlClientFactory.Build("serial://./COM1?BaudRate=9600");
+urlClient.Open();
+Console.ReadLine();
+urlClient.Close();
--- src/Quick.UrlClient.SerialPort/SerialPortUrlClientOptions.cs
+++ src/Quick.UrlClient.SerialPort/SerialPortUrlClientOptions.cs
@@ -6,17 +6,27 @@ public class SerialPortUrlClientOptions
     public int BaudRate { get; set; }
     public string Parity { get; set; } = nameof(System.IO.Ports.Parity.None);
     public int DataBits { get; set; } = 8;
-    public string StopBits { get; set; } = nameof(System.IO.Ports.StopBits.None);
+    public string StopBits { get; set; } = nameof(System.IO.Ports.StopBits.One);
 
     public int? WriteTimeout { get; set; }
     public int? ReadTimeout { get; set; }
     public int? WriteBufferSize { get; set; }
     public int? ReadBufferSize { get; set; }
+    private const string unixPortNamePrefix = "/dev";
 
     public SerialPortUrlClientOptions() { }
     public SerialPortUrlClientOptions(Uri uri)
     {
-        PortName = uri.Host;
+        PortName = uri.AbsolutePath;
+        if (OperatingSystem.IsWindows())
+        {
+            PortName = PortName.Trim('/');
+        }
+        else
+        {
+            if (!PortName.StartsWith(unixPortNamePrefix))
+                PortName = unixPortNamePrefix + PortName;
+        }
         var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
         foreach (var key in query.AllKeys)
         {
